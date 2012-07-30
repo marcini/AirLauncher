db2c32de9cf1556b6fb9d13dccf5d06b37260e74
user_2 <user_2@192.168.1.101>
2012-07-30 23:39:47 +0200

    initial app

--- js/Application.js
+++ js/Application.js
@@ -1,7 +1,18 @@
 var Application = function(){
-    
+    this.arguments = [];  
+    this.connectionManager = null;
 }
 
 Application.prototype.init = function(){
-  air.trace('start');
+  
+  air.NativeApplication.nativeApplication.addEventListener(air.InvokeEvent.INVOKE, $.proxy(this.onInvoke, this));
+  
+}
+
+Application.prototype.onInvoke = function(evt){
+    if (evt.arguments.length){
+        this.arguments = evt.arguments;
+        this.connectionManager = new ConnectionManager();
+        this.connectionManager.fetchDocument();
+    }    
 }
--- js/ConnectionManager.js
+++ js/ConnectionManager.js
@@ -1,6 +1,22 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var ConnectionManager = function(){
+    
+    this.startUrl = null;
+    
+    this.passParams = {
+        switcher: null,
+        id_procesu: null,
+        id_common: null
+    }
+    
+}
 
+ConnectionManager.prototype.fetchDocument = function(startUrl){
+    //TODO polaczenie z autoryzacja, jak z launchera
+    //http://help.adobe.com/en_US/air/html/dev/WS5b3ccc516d4fbf351e63e3d118a9b8f6c0-7ffe.html
+    
+    //TODO zainicjalizowac DocumentHandler, wywolac otwarcie dokumentu i podlaczyc sie pod event modyfikacji
+}
 
+ConnectionManager.prototype.sendDocument = function(){
+    
+}
